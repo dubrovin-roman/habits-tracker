5f200f5bf9f8251cdede54a9670f7de4ca8b7451
Roman <dubrovin.skfo@gmail.com>
2023-01-08 17:58:29 +0300

    загрузка и сохранение данных

--- js/app.js
+++ js/app.js
@@ -1,1 +1,21 @@
 'use strict';
+
+let habbits = [];
+const HABBITS_KEY = 'HABBITS_KEY';
+
+/* utils */
+function loudData() {
+    const stringHabbits = localStorage.getItem(HABBITS_KEY);
+    const arrayHabbits = JSON.parse(stringHabbits);
+    if (Array.isArray(arrayHabbits)) {
+        habbits = arrayHabbits;
+    }
+}
+
+function saveData() {
+    localStorage.setItem(HABBITS_KEY, JSON.stringify(habbits));
+}
+
+(() => {
+    loudData();
+})();
